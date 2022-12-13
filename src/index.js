c3d61e06927e01311c4b9126ed2171286a4cd5cb
Rocio Quigua <rocioquigualopez@gmail.com>
2022-12-12 23:39:43 -0500

    feat(js): update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './scenes/App'; 
+
+import App  from './scenes/App'; 
+import './sass/main.scss';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/scenes/App.js
+++ src/scenes/App.js
@@ -1,38 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import api  from '../common/api/api';
+import { Home } from '../scenes/Home/Home';
 
-export const App = () => {
+const App = () => {
   
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    handleData();
-  }, []);
-
-  const handleData = async () =>{
-    const data = await api.get("/character");
-    setCharacters(data.res.results);
-    console.log(data);
-  };
-
   return (
-    <div>
-      <h1>Personajes</h1>
-      <ul>
-        {characters.map(item => (
-          <li>
-            <h2>{item.name}</h2>
-            <img src={item.image} alt="foto del personaje"/>
-            <p>{item.species}</p>
-            <p>{item.status}</p>
-            <p>{item.origin.name}</p>
-          </li>
-        ))}
-      </ul>
+    <div> 
+      <Home />
     </div>
   )
 };
 
-
+export default App;
 
